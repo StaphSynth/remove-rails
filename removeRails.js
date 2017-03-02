cf42448fe0955ec20287f6b312c14bb3aa43f8bc
Staphsynth <1337dallen@gmail.com>
2017-03-02 17:00:30 +1100

    render buttons for surveys

--- removeRails.js
+++ removeRails.js
@@ -2,18 +2,25 @@ var listApp = angular.module('removeRails', []);
 
 listApp.controller('removeRailsController', function($http){
   var ctrl = this;
+  ctrl.root = 'http://127.0.0.1:8000';
+  ctrl.surveyIndices = [];
 
-  $http.get('http://127.0.0.1:8000/index.json')
-    .then(function(response) {
-      ctrl.handleResponse(response);
+  ctrl.getData = function(url, processData) {
+    $http.get(ctrl.root + url)
+      .then(function(response) {
+        if(response.status != 200) {
+          //report a status error
+        } else {
+          //do something useful with the data
+          processData(response.data);
+        }
     });
-
-  ctrl.handleResponse = function(response) {
-    if(response.status != 200) {
-      //report a status error
-    } else {
-      //call a function to do something useful with the data
-    }
   };
+
+  ctrl.surveyIndex = function(data) {
+    ctrl.surveyIndices = data.survey_results;
+  };
+
+ctrl.getData('/index.json', ctrl.surveyIndex);
 
 });
