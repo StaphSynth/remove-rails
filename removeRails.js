f46fb467d8c7e11a5b41e46b8a50f23131c1245d
Staphsynth <1337dallen@gmail.com>
2017-03-03 14:45:27 +1100

    display survey meta data

--- removeRails.js
+++ removeRails.js
@@ -4,6 +4,11 @@ listApp.controller('removeRailsController', function($http){
   var ctrl = this;
   ctrl.root = 'http://127.0.0.1:8000';
   ctrl.surveyIndices = [];
+  ctrl.surveyDetails = {
+    name: null,
+    participantCount: null,
+    submittedResponseCount: null
+  };
 
   ctrl.getData = function(url, processData) {
     $http.get(ctrl.root + url)
@@ -19,6 +24,12 @@ listApp.controller('removeRailsController', function($http){
     ctrl.surveyIndices = data.survey_results;
   };
 
-ctrl.getData('/index.json', ctrl.surveyIndex);
+  ctrl.constructDisplay = function(data) {
+    ctrl.surveyDetails.name = data.survey_result_detail.name;
+    ctrl.surveyDetails.participantCount = data.survey_result_detail.participant_count;
+    ctrl.surveyDetails.submittedResponseCount = data.survey_result_detail.submitted_response_count;
+  };
+
+  ctrl.getData('/index.json', ctrl.surveyIndex);
 
 });
