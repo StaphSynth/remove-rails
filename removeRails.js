f98c3105ffcc78ecc80efe3499598596abbd23c8
Staphsynth <1337dallen@gmail.com>
2017-03-07 13:22:40 +1100

    refactored dataop factory

--- removeRails.js
+++ removeRails.js
@@ -8,48 +8,37 @@ var rrApp = angular.module('removeRails', []);
 
 rrApp.controller('removeRailsController', function(DataOp){
   var ctrl = this;
-  ctrl.surveyIndices = [];
-  ctrl.surveyDetails = {
-    name: null,
-    participantCount: null,
-    submittedResponseCount: null
-  };
 
-  function getData(path, processData) {
-    DataOp.getData(path)
+  DataOp.updateSurveyIndex('/index.json');
+  ctrl.surveyIndices = DataOp.getSurveyIndex;
+});
+
+rrApp.factory('DataOp', function($http) {
+  var urlRoot = 'http://127.0.0.1:8000';
+  var surveyIndex = [];
+
+  function getData(path, callBack) {
+    $http.get(path)
       .then(function(response) {
-        processData(response.data);
+        callBack(response.data);
       })
       .catch(function(error) {
         console.log(error);
         alert('Error!\nCode: ' + error.status.toString() + '\n' + error.statusText);
       });
-
   }
 
-  ctrl.surveyIndex = function(data) {
-    ctrl.surveyIndices = data.survey_results;
-  };
+  function setSurveyIndex(data) {
+    surveyIndex = data.survey_results;
+  }
 
-  ctrl.constructDisplay = function(data) {
-    ctrl.surveyDetails.name = data.survey_result_detail.name;
-    ctrl.surveyDetails.participantCount = data.survey_result_detail.participant_count;
-    ctrl.surveyDetails.submittedResponseCount = data.survey_result_detail.submitted_response_count;
+  return {
+    updateSurveyIndex : function(path) {
+      getData(urlRoot + path, setSurveyIndex);
+    },
 
-    console.log(data);
-  };
-
-  getData('/index.json', ctrl.surveyIndex);
-
-});
-
-rrApp.factory('DataOp', function($http) {
-  var urlRoot = 'http://127.0.0.1:8000';
-  var DataOp = {};
-
-  DataOp.getData = function(path) {
-    return $http.get(urlRoot + path);
-  };
-
-  return DataOp;
+    getSurveyIndex : function() {
+      return surveyIndex;
+    }
+  }
 });
