8e2df93b0530db0114aa01382999156372a0ac44
Staphsynth <1337dallen@gmail.com>
2017-03-08 11:51:11 +1100

    data loading now operates spinners properly

--- removeRails.js
+++ removeRails.js
@@ -11,16 +11,27 @@ rrApp.controller('removeRailsController', function(DataOp){
 
   DataOp.updateSurveyIndex('/index.json');
   ctrl.surveyIndices = DataOp.getSurveyIndex;
+  ctrl.loading = DataOp.getLoadingStatus;
+  ctrl.loadSurvey = function(url) {
+    DataOp.fetchSurveyData(url);
+  };
+  ctrl.getSurveyData = DataOp.getSurveyData;
 });
 
 rrApp.factory('DataOp', function($http) {
   var urlRoot = 'http://127.0.0.1:8000';
-  var surveyIndex = [];
+  var surveyIndex = null;
+  var surveyData = null;
+  var loading = false;
 
   function getData(path, callBack) {
+    loading = true;
     $http.get(path)
       .then(function(response) {
         callBack(response.data);
+      })
+      .then(function() {
+        loading = false;
       })
       .catch(function(error) {
         console.log(error);
@@ -28,17 +39,31 @@ rrApp.factory('DataOp', function($http) {
       });
   }
 
-  function setSurveyIndex(data) {
-    surveyIndex = data.survey_results;
-  }
-
   return {
     updateSurveyIndex : function(path) {
-      getData(urlRoot + path, setSurveyIndex);
+      surveyIndex = null;
+      getData(urlRoot + path, function(data){
+        surveyIndex = data.survey_results;
+      });
+    },
+
+    fetchSurveyData : function(url) {
+      surveyData = null;
+      getData(urlRoot + url, function(data) {
+        surveyData = data.survey_result_detail;
+      });
     },
 
     getSurveyIndex : function() {
       return surveyIndex;
+    },
+
+    getSurveyData : function() {
+      return surveyData;
+    },
+
+    getLoadingStatus : function() {
+      return loading;
     }
   }
 });
