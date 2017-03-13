4b7c03a49a1e11791699642a8887a74e92f6ad29
StaphSynth <StaphSynth@users.noreply.github.com>
2017-03-13 16:10:22 +1100

    update test-spec

--- app/js/tests/test.spec.js
+++ app/js/tests/test.spec.js
@@ -1,5 +1,5 @@
 describe('testing the test framework', function() {
-  it('tests the test framework', function() {
+  it('tests that 1 == 1', function() {
     var x = 1;
     expect(x).toEqual(1);
   });
--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'app/js/*.*.js',
+      // 'app/js/*.*.js',
       'app/js/tests/*.spec.js'
     ],
 
