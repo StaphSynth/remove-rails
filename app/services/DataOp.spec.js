969f195179c50a0b43cfbdb6c5d561fcd085d43e
Staphsynth <1337dallen@gmail.com>
2017-03-14 14:26:55 +1100

    first proper angular service test spec

--- app/services/DataOp.spec.js
+++ app/services/DataOp.spec.js
@@ -1,5 +1,19 @@
-describe('Test the framework (redux)', function() {
-  it('tests that 1 + 1 == 2', function() {
-    expect(1 + 1).toEqual(2);
+//Test suite for DataOp factory
+
+describe('DataOp Factory', function() {
+  var dataOp;
+
+  //load the module
+  beforeEach(angular.mock.module('removeRails'));
+
+  //inject dependancies
+  beforeEach(inject(function(_DataOp_) {
+    dataOp = _DataOp_;
+  }));
+
+  //test specs
+
+  it('should exist', function() {
+    expect(dataOp).toBeDefined();
   });
 });
