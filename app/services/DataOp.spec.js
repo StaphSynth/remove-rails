9b17f0ad6b9583915f241a5f39c246e2f83db083
Staphsynth <1337dallen@gmail.com>
2017-03-14 16:28:43 +1100

    playing with dependancy injection

--- app/services/DataOp.spec.js
+++ app/services/DataOp.spec.js
@@ -1,19 +1,40 @@
 //Test suite for DataOp factory
 
 describe('DataOp Factory', function() {
-  var dataOp;
+  var DataOp;
 
   //load the module
   beforeEach(angular.mock.module('removeRails'));
 
-  //inject dependancies
+  //inject dependancy
   beforeEach(inject(function(_DataOp_) {
-    dataOp = _DataOp_;
+    DataOp = _DataOp_;
   }));
 
   //test specs
 
   it('should exist', function() {
-    expect(dataOp).toBeDefined();
+    expect(DataOp).toBeDefined();
   });
+
+  describe('DataOp methods', function() {
+
+    //instantiate global
+    var $http;
+
+    //inject dependancy
+    beforeEach(inject(function(_$http_) {
+      $http = _$http_;
+    }));
+
+    describe('getLoadingStatus method', function() {
+
+      it('should return false when the app is idle', function() {
+        expect(DataOp.getLoadingStatus()).toEqual(false);
+      });
+    });
+
+
+  });
+
 });
