847d4658e5ccea43b2c3bf186509050eb2fa177b
Staphsynth <1337dallen@gmail.com>
2017-03-15 12:32:54 +1100

    begin writing e2e tests

--- app/e2e/e2e.spec.js
+++ app/e2e/e2e.spec.js
@@ -1,13 +1,17 @@
-describe('testing the e2e framework', function() {
-  it('should add an item to my to-do list app', function() {
-    browser.get('http://syntheta.se/coding/2017/02/21/AngularJS-todo-list.html');
+//End-to-end test spec for remove-rails
 
-    element(by.css('input.taskInput')).sendKeys('learn to use protractor');
-    element(by.css('input.submitButton')).click();
+//these tests expect the app to be live and running at localhost:8000
+describe('E2E testing of the remove rails app', function() {
 
-
-    var todoList = element.all(by.repeater('item in ctrl.list'));
-    expect(todoList.count()).toEqual(1);
-    expect(todoList.get(0).element(by.css('span.task')).getText()).toEqual('learn to use protractor');
+  beforeEach(function() {
+    browser.get('http://127.0.0.1:8000');
   });
+
+  it('should exist and contain a heading', function() {
+    expect(element(by.css('h1')).getText()).toEqual('Survey Results');
+  });
+
+  // it('should load index.json', function() {
+  //   //do stuff
+  // });
 });
